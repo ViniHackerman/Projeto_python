f610adca10e64698ebcf085a47ca6bd8effb708f
ViniHackerman <viniciusgouveamachado@gmail.com>
2024-02-06 22:40:37 -0300

    Percorrer arquivos da pasta base de dados

---
+++ main.py
@@ -0,0 +1,12 @@
+#logica de programação
+# 1 percorrer todos os arquivos da pasta base de dados(pasta vendas)
+
+import os
+lista_arquivos = os.listdir("C:\\Users\\vinic\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas")
+
+print(lista_arquivos)
+# 2 imporar as bases de dados de vendas
+# 3 tratar compilar as bases de dados
+# 4 calcular o produto mais vendido (quantidade) 
+# 5 calcular o produto que mais faturou
+# 6 calcular a loja/cidade que mais faturou - grafico
