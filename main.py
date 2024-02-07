4de9fe8de6bcc97870eca610f93798a09a231702
ViniHackerman <viniciusgouveamachado@gmail.com>
2024-02-07 12:58:16 -0300

    Laços de repetição nos arquivos

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import os
 lista_arquivos = os.listdir("C:\\Users\\vinic\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas")
 
 print(lista_arquivos)
-# 2 imporar as bases de dados de vendas
+# 2 importar as bases de dados de vendas
+ 
+for arquivo in lista_arquivos:
+    print(f"C:\\Users\\vinic\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas{arquivo}")
+
 # 3 tratar compilar as bases de dados
 # 4 calcular o produto mais vendido (quantidade) 
 # 5 calcular o produto que mais faturou
