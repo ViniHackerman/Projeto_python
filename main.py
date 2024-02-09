20d81a4ef22c9c0c424023eca59a34b69b52c70e
ViniHackerman <viniciusgouveamachado@gmail.com>
2024-02-08 22:20:34 -0300

    Fazendo tabelas e bases de dados em python

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 #logica de programação
 # 1 percorrer todos os arquivos da pasta base de dados(pasta vendas)
-
+from IPython.display import display
+import pandas as pd
 import os
-lista_arquivos = os.listdir("C:\\Users\\vinic\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas")
 
-print(lista_arquivos)
-# 2 importar as bases de dados de vendas
- 
+pasta_vendas = "C:\\Users\\vinic\\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas"
+lista_arquivos = os.listdir(pasta_vendas)
+
+tabela_total = pd.DataFrame()
+# Importar as bases de dados de vendas
 for arquivo in lista_arquivos:
-    print(f"C:\\Users\\vinic\Downloads\\01-15 - Curso de Python - Aula 1\\Vendas{arquivo}")
+    caminho_arquivo = os.path.join(pasta_vendas, arquivo)
+    tabela = pd.read_csv(caminho_arquivo)
+    tabela_total=tabela_total._append(tabela)
+
+    
 
 # 3 tratar compilar as bases de dados
+display(tabela_total)
 # 4 calcular o produto mais vendido (quantidade) 
 # 5 calcular o produto que mais faturou
 # 6 calcular a loja/cidade que mais faturou - grafico
